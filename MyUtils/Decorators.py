58615355eb72291037ecc3dde5e00a4f9d3b22da
bwrob <43605200+bwrob@users.noreply.github.com>
2023-08-30 20:05:44 +0200

    [CHANGE]  clean up and update the decorators

--- MyUtils/Decorators.py
+++ MyUtils/Decorators.py
@@ -1,28 +1,19 @@
 from time import perf_counter
-from functools import wraps
+from functools import wraps, partial
 
-FUNCTIONS = dict()
+
 DEFAULT_VALUE_DEBUG_MODE = True
 DEFAULT_VALUE_PRINT_VALUE = False
 DEFAULT_VALUE_TIMER = True
 
 
-def time(func):
-    """
-    Print the runtime of the decorated function
-    """
-    @wraps(func)
-    def handler(*args, **kwargs):
-        start_time = perf_counter()
-        output = func(*args, **kwargs)
-        end_time = perf_counter()
-        duration = end_time - start_time
-        print(f"\nRuntime of {func.__name__!r} was {duration} seconds.")
-        return output
-    return handler
-
-
 def debugging(debug_mode, print_call_output, timer):
+    """
+    :param debug_mode: flag to gather debugging info during computation
+    :param print_call_output: flag to print call and output info for the wrapped function
+    :param timer: flag to time the execution time
+    :return: decorated function
+    """
     def wrapper(func):
         @wraps(func)
         def wrapped(*args, **kwargs):
@@ -33,8 +24,10 @@ def debugging(debug_mode, print_call_output, timer):
                 print(f"Calling {func.__name__}({signature})")
             if timer:
                 tic = perf_counter()
+
             # actual function call
             result = func(debug_mode, *args, **kwargs)
+
             if timer:
                 toc = perf_counter()
                 print("{} evaluated in {} seconds".format(func.__name__, toc - tic))
@@ -46,38 +39,18 @@ def debugging(debug_mode, print_call_output, timer):
 
 
 def debug(debug_mode=DEFAULT_VALUE_DEBUG_MODE,
-          print_value=DEFAULT_VALUE_PRINT_VALUE,
+          print_call_output=DEFAULT_VALUE_PRINT_VALUE,
           timer=DEFAULT_VALUE_TIMER,
           ):
     """
-    Decorator. If no debuging flags are enabled, just return the function.
+    Decorator for debugging purposes.
+    If no debugging flags are enabled, just return the function.
+    :param debug_mode: flag to gather debugging info during computation
+    :param print_call_output: flag to print call and output info for the wrapped function
+    :param timer: flag to time the execution time
+    :return: decorated function
     """
-    if any(control := (debug_mode, print_value, timer)):
+    if any(control := (debug_mode, print_call_output, timer)):
         return debugging(*control)
     else:
-        return lambda x: x
-
-
-def register(func):
-    """
-    Register a function as available to use
-    """
-    FUNCTIONS[func.__name__] = func
-    return func
-
-
-def memo(func):
-    """
-    Keep a cache of previous function calls (memoization)
-    This is just mock up better use @functools.lru_cache(maxsize=)
-    """
-
-    @wraps(func)
-    def wrapper_memoize(*args, **kwargs):
-        cache_key = args + tuple(kwargs.items())
-        if cache_key not in wrapper_memoize.cache:
-            wrapper_memoize.cache[cache_key] = func(*args, **kwargs)
-        return wrapper_memoize.cache[cache_key]
-
-    wrapper_memoize.cache = dict()
-    return wrapper_memoize
+        return lambda x: partial(x, debug=False)
