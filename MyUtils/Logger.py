95b1b03c15c15638eb8f5b77184708a8ca5732c8
bwrob <43605200+bwrob@users.noreply.github.com>
2023-08-31 15:54:04 +0200

    [CHANGE] MyLogger as Singleton metaclass

--- MyUtils/Logger.py
+++ MyUtils/Logger.py
@@ -1,61 +1,54 @@
 import os
-import logging
+from logging import *
 from datetime import datetime
 from contextlib import contextmanager
+from MyUtils.Singleton import Singleton
 
 
-def get_logger(logger_name, dir_path=None, file_name=''):
-    formatter = logging.Formatter("%(asctime)s - %(message)s")
+class MyLogger(Logger, metaclass=Singleton):
+    """
+    Wraps standard logging library logger into singleton.
+    """
 
-    # setup file handler
-    if dir_path is None:
-        dir_path = './txt'
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
-    file_name = file_name.format(datetime.now())
-    file_path = os.path.join(dir_path, file_name)
-    file_handler = logging.FileHandler(file_path)
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(formatter)
+    def __init__(self, logger_name='', dir_path=None, file_name='log_{}.log'):
 
-    # setup stream handler
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.INFO)
-    console_handler.setFormatter(formatter)
+        super().__init__(logger_name)
+        formatter = Formatter("%(asctime)s - %(message)s")
 
-    logger = logging.getLogger(logger_name)
-    logger.setLevel(logging.DEBUG)
-    logger.handlers = [file_handler, console_handler]
+        # setup file handler
+        if dir_path is None:
+            dir_path = './'
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        file_name = file_name.format(datetime.now().date())
+        file_path = os.path.join(dir_path, file_name)
+        file_handler = FileHandler(file_path)
+        file_handler.setLevel(DEBUG)
+        file_handler.setFormatter(formatter)
 
-    return logger
+        # setup stream handler
+        console_handler = StreamHandler()
+        console_handler.setLevel(INFO)
+        console_handler.setFormatter(formatter)
 
-
-class Logger:
-    instance = None
-
-    def __init__(self, logger=None):
-        if self.instance is not None:
-            self.instance = logger
-
-    def log(self, msg, level=logging.INFO):
-        if self.instance is not None:
-            self.instance.log(level, msg)
-        else:
-            print(datetime.now(), ' | ', msg)
+        self.setLevel(DEBUG)
+        self.handlers = [file_handler, console_handler]
 
     @contextmanager
-    def log_duration(self, task_name, level=logging.INFO):
-        self.log('Started ' + task_name, level)
+    def log_duration(self, task_name, level=INFO):
+        self.log(msg='DURATION | Started ' + task_name, level=level)
         yield None
-        self.log('Ended ' + task_name, level)
+        self.log(msg='DURATION | Ended ' + task_name, level=level)
 
-    def log_error(self, msg, level=logging.INFO):
-        self.log('ERROR | ' + msg, level)
+    def log_error(self, error_msg):
+        self.exception(msg='ERROR | ' + error_msg)
+
+    def log_info(self, msg):
+        self.log(msg=msg, level=INFO)
+
+    def log_debug(self, msg):
+        self.log(msg=msg, level=DEBUG)
 
 
-def init_logger(logger_name, dir_path=None, file_name=''):
-    logger = get_logger(logger_name, dir_path=dir_path, file_name=file_name)
-    Logger(logger)
-
-
-theLogger = Logger()
+theLogger = MyLogger('log')
+theLogger.log_debug('Logger initialized.')
